1aa0c1fdce1efef8279ad73574304e634348d8d0
iohri <iohri1360@gmail.com>
2018-04-27 16:28:22 +0300

    Menu Design
    Menu is created and added to the project
    
    Co-Authored-By: iremoge <iremoge@users.noreply.github.com>

---
+++ SeniorProject-AR/Assets/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public void OrientationMode()
+    {
+        SceneManager.LoadScene("OrientationMode");
+    }
+
+    public void SystemDashboard()
+    {
+        SceneManager.LoadScene("SystemDashboard");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("QUIT !");
+        Application.Quit();
+    }
+}
